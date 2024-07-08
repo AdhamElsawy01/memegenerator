af4588fda7726137dd4e1d09524389d077979579
AdhamElsawy01 <elsawyadham351@gmail.com>
2024-07-08 18:38:47 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const endpoint = "https://meme-api.com/gimme/wholesomememes";
+
+const memeParent = document.querySelector(".meme-parent");
+const generateBtn = document.querySelector(".generate-btn");
+const memeImg = document.querySelector(".meme-img");
+const errorParent = document.querySelector(".error-parent");
+const likesParent = document.querySelector(".likes-parent");
+
+generateBtn.onclick = function() {
+    generateMeme()
+    .then((response) => {
+        memeParent.classList.remove("hide");
+        memeImg.src = response.url;
+        likesParent.textContent = `${response.ups} person liked this shit`;
+    })
+    .catch((response) => {
+        errorParent.classList.remove("hide");
+        memeParent.classList.add("hide");
+        errorParent.textContent = response;
+    });
+};
+
+function generateMeme() {
+    return new Promise(function(resolve,reject) {
+        const memeRequest = new XMLHttpRequest();
+        memeRequest.open("GET",endpoint);
+        memeRequest.send();
+
+        memeRequest.onload = function() {
+            if (this.status === 200) {
+                resolve(JSON.parse(memeRequest.response));
+            } else {
+                reject("An Error Occured");
+            };
+        };
+    });
+};
